33b1ac0b20d273657af01fcf193c907e9b815846
Unknown <ac.gonzalesllerena@gmail.com>
2019-03-24 13:02:41 -0500

    config aws

--- src/main/java/com/intercorp/config/MvcConfig.java
+++ src/main/java/com/intercorp/config/MvcConfig.java
@@ -1,4 +1,4 @@
-package com.aws.codestar.projecttemplates.configuration;
+package com.intercorp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
